d25dd448b0c9f17d10b23b65de8f799f2baa31d8
Ana-Brengle <Mariana_Brengle@insideranken.org>
2024-10-11 14:38:14 -0500

    added code

--- homework/JS_CH02_HW/burger.js
+++ homework/JS_CH02_HW/burger.js
@@ -1,10 +1,14 @@
 document.getElementById('btnClick').onclick = () => {
 
-  const burgers = parseInt(document.getElementById('burgers'));
+  const burgers = parseInt(document.getElementById('burgers').value);
 
-  const fries = parseInt(document.getElementById('fries'));
+  const fries = parseInt(document.getElementById('fries').value);
 
-  const sodas = parseInt(document.getElementById('sodas'));
+  const sodas = parseInt(document.getElementById('sodas').value);
 
-   const total = (burgers * 1) + (fries * 2) + (sodas + 2.50);
+   const total = (burgers * 1) + (fries * 2) + (sodas * 2.50);
+
+   const totdisplay = `Here's your total: $${total}`;
+   
+   document.getElementById('total').innerHTML = totdisplay;
 };
